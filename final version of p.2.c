a9d287ac6639d028ce8a2352fed9ece316747bbf
Reihane2783 <reihane2783@gmail.com>
2025-09-25 01:56:54 +0330

    p.2.c

--- final version of p.2.c
+++ final version of p.2.c
@@ -3,20 +3,40 @@
 #include <math.h>
 #include <time.h>
 
-double f(int x,int y);
-int main()
-{double sum=0;
-  for (int i=(-10);i<=10;i++)
-       {sum+=((f(i,i+1))/(5+ f(2*i,i)));}
-printf("The result is %f\n",sum);
-    return 0;}
+// Function prototype
+double f(int x, int y);
 
-double f(int x,int y)
-{double z=0,p;
-srand(time(NULL));
-if(x<=y)
-    z=(rand()%(y-x+1))+x;
-else
-    z=(rand()%(x-y+1))+y;
-p=sqrt(pow(abs(x-y),1/3)+pow((y-z),2));
-return p;}
+int main()
+{
+    double sum = 0;
+
+    // Loop from -10 to 10 and accumulate the result of the expression
+    for (int i = -10; i <= 10; i++)
+    {
+        sum += f(i, i + 1) / (5 + f(2 * i, i));
+    }
+
+    printf("The result is %f\n", sum);
+    return 0;
+}
+
+// Function f(x, y) generates a random number z between x and y,
+// then computes the expression: sqrt( |x - y|^(1/3) + (y - z)^2 )
+double f(int x, int y)
+{
+    double z = 0, p;
+
+    // Seed the random number generator
+    srand(time(NULL));
+
+    // Generate a random integer z between x and y (inclusive)
+    if (x <= y)
+        z = (rand() % (y - x + 1)) + x;
+    else
+        z = (rand() % (x - y + 1)) + y;
+
+    // Compute the expression
+    p = sqrt(pow(abs(x - y), 1.0 / 3.0) + pow((y - z), 2));
+
+    return p;
+}
